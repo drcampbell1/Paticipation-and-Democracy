7c5b31cd8e1ac04fab8c51cdeb1dcd020d6f27a8
Dr Ross Campbell <36095902+drcampbell1@users.noreply.github.com>
2020-09-15 21:00:36 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,10 +1,10 @@
 # Install all packages
-library(remotes)
+#library(remotes)
 # install_version("tibble", "3.0.3")
 # install_version("readr", "1.3.1")
 # install_version("dplyr", "1.0.0")
 # install_version("ggplot2", "3.3.2")
 # install_version("ggcorrplot", "0.1.3")
 # install_version("plotly", "4.9.2.1")
-install_version("sjPlot", "2.8.4")
+#install_version("sjPlot", "2.8.4")
 
