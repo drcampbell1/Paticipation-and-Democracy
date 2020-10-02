4b2efa8281b9f9741c88912cc8a8603f603c0ae2
Dr Ross Campbell <36095902+drcampbell1@users.noreply.github.com>
2020-10-02 11:16:52 +0100

    Add files via upload

---
+++ Lab Exercises/Lab_4_Radical_Right.R
@@ -0,0 +1,149 @@
+# In the lecture, we introduced theories about who votes for the radical right
+
+# In this lab, we're going to test some aspects of those theories with evidence from the ESS
+
+ESS <- foreign::read.dta("data/ESS.dta", convert.factors=TRUE)
+attach(ESS)
+View(ESS)
+library(tidyverse)
+
+ESS %>% filter(!is.na(rright)) %>% count(rright) %>%
+  mutate("%" = round(n/sum(n) * 100, digits=1)) %>%
+  knitr::kable("pandoc", caption = "Voted for the Radical Right in European Democracies",
+               col.names = c('Voted Radical Right', 'N', '%'), align="lccc")
+
+#How does this vary by country?#
+
+ESS %>% group_by(cntry) %>% filter(!is.na(rright)) %>%
+     count(rright) %>% mutate("%" = round(n/sum(n) * 100, digits=1)) %>% filter(!rright =="no") %>% 
+     knitr::kable("pandoc", caption = "Comparing Voting for Radical Right in European Democracies",
+     col.names = c('Country', 'Voted Radical Right', 'N', '%'), align="cccc")
+
+
+# Let's graph this to make the findings stand out a bit better
+
+ESS %>% group_by(cntry) %>% filter(!is.na(rright)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=reorder(cntry, -prop), y=prop, fill=cntry)) +
+  geom_bar(stat="identity")+
+  scale_fill_manual(values=c("#E69F00", "#009E73", "#0072B2"))+
+  labs(x="", y="", title="Figure 1: Voted for Radical Right Party by Country", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+
+# What is the age profile of voters for the radical right?
+
+a <- ESS %>% filter(cntry %in% c("Germany", "France", "UK")) %>% 
+             group_by(cntry) %>% 
+             summarise(mean= mean(age, na.rm=T))
+b <- ESS %>% filter(cntry %in% c("Germany", "France", "UK")) %>%
+             group_by(cntry, rright) %>% summarise(avg_age_vt_rr= mean(age, na.rm=T)) %>% 
+             filter(!is.na(rright), !rright=="no")
+
+left_join(a, b, by = "cntry") %>% select(-rright) %>%
+          knitr::kable("pandoc", caption = "Average Age of Population and Voters of Radical Right Parties in European Democracies",
+          col.names = c('Country', 'Average Age Population', 'Average Age Voter Radical Right Party'), digits =1, align="cccc")
+
+# What does this tell us about the age profile of voters for radical right parties?
+
+
+
+# Let's look at the gender profile of radical right voters
+
+
+c <- ESS %>% filter(cntry %in% c("Germany", "France", "UK")) %>%
+  group_by(cntry) %>%
+  count(gender) %>%
+  mutate(perc_pop=n/sum(n)*100)
+
+
+d <- ESS %>% ESS %>% filter(cntry %in% c("Germany", "France", "UK")) %>%
+  group_by(cntry, gender) %>% filter(!is.na(rright)) %>%
+  count(rright) %>% mutate(perc_vt_pp=n/sum(n)*100) %>%
+  filter(!rright=="no") %>%
+  select(-rright)
+
+left_join(c, d, by=c("cntry", "gender")) %>% select(-n.x, -n.y) %>%
+  knitr::kable("pandoc", caption = "Gender Balance in Population and Voters of Radical Right Parties in European Democracies",
+               col.names = c('Country', 'Gender', 'Percentage Population', 'Percentage Voter of Radical Right Political Party'),
+               digits =1, align="cccc") %>% print()
+
+rm(a,b,c,d)
+
+
+#Let's look at Education
+
+ESS %>% group_by(educat) %>% filter(!is.na(rright), !is.na(educat)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=educat, y=prop, fill=educat)) +
+  geom_bar(stat="identity")+
+  labs(x="", y="", title="Figure 2: Voting for the Radical Right by Education", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+ESS %>% group_by(educat, cntry) %>% filter(!is.na(rright), !is.na(educat)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=educat, y=prop, fill=educat)) +
+  geom_bar(stat="identity")+
+  facet_grid(~cntry)+
+  labs(x="", y="", title="Figure 3: Voting for the Radical Right by Education and Country", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+#Let's look at what voters of the radical right think about government?
+
+#Do voters of the radical right trust government?
+
+ESS %>% group_by(ptrust) %>% filter(!is.na(rright), !is.na(ptrust)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=ptrust, y=prop, fill=ptrust)) +
+  geom_bar(stat="identity")+
+  labs(x="", y="", title="Figure 4: Political Trust and Voting for the Radical Right", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+ESS %>% group_by(ptrust, cntry) %>% filter(!is.na(rright), !is.na(ptrust)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=ptrust, y=prop, fill=ptrust)) +
+  geom_bar(stat="identity")+
+  facet_grid(~cntry)+
+  labs(x="", y="", title="Figure 5: Political Trust and Voting for the Radical Right", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+
+# Trust in European Parliament
+
+ESS %>% group_by(tr_ep) %>% filter(!is.na(rright), !is.na(tr_ep)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=tr_ep, y=prop, fill=tr_ep)) +
+  geom_bar(stat="identity")+
+  labs(x="", y="", title="Figure 6: Trust in the European Parliament and Voting for the Radical Right", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+ESS %>% group_by(tr_ep, cntry) %>% filter(!is.na(rright), !is.na(tr_ep)) %>%
+  count(rright) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!rright=="no") %>%
+  ggplot(aes(x=tr_ep, y=prop, fill=tr_ep)) +
+  geom_bar(stat="identity")+
+  facet_grid(~cntry)+
+  labs(x="", y="", title="Figure 7: Trust in the European Parliament and Voting for the Radical Right", caption="ESS 2016")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  guides(fill=FALSE)
+
+# What can we conclude about voters for the radical right?
